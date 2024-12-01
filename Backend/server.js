dfc7e4c0112eca5834becccdd02e5d0fd22f75b7
youssefescobar <89347154+youssefescobar@users.noreply.github.com>
2024-12-01 06:37:56 +0200

    added db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const testroute = require("./routes/testroute");
+const connectDB = require('./config/db');
+require('dotenv').config();
+
 const app = express();
+
 app.use(express.json());
+connectDB();
 app.use("/api/test", testroute);
 
 app.listen(3000, () => {
